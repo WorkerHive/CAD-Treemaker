8fb87c504d9e07994dc9e49eedfd584aef100540
Dominic Martindale <awalletsdream@gmail.com>
2020-12-24 00:43:59 +1300

    Init not working
    Everything is in the works. Continuing this tomorrow morning.

---
+++ src/index.js
@@ -0,0 +1,84 @@
+
+/*
+
+PSEUDO CODE
+
+class Treenode:
+    int id;
+    Treenode parent;
+    Treenode [] children;
+
+function rootTree(g, rootId = 0):
+    root = Treenode(rootId, null, [])
+    return buildTree(g, root, null)
+
+function buildTree(g, node, parent):
+    for child in g[node.id]:
+        if parent != null and childId == parent.id:
+            continue
+        child = Treenode(childId, node, [])
+        node.children.add(child)
+        buildTree(g, child, node)
+    return node
+    
+*/
+
+/*
+ * The parent node is the node which we choose to order all other nodes by.
+ * See: https://towardsdatascience.com/graph-theory-rooting-a-tree-fb2287b09779 [accessed: 23/12/2020]
+ * 
+ * This process uses Depth-first search [DFS] to order the tree object output from the .glb input.
+ */
+
+/*
+//Our Parent Node
+const TreeRoot;
+const Seed = AmorphusTreeEntity
+
+class TreeNode {
+    constructor(id, parent, children) {
+        this.id = id,
+        this.parent = parent,
+        this.children = children
+      }
+
+    id() {
+        return this.id; 
+    }
+
+    parent() {
+        return this.parent
+    }
+
+    children(){
+        return this.children
+    }
+}
+
+function GrowTree(tree){
+    //process tree
+    let processedtree = tree
+    let branches = processedtree
+
+    branches.forEach(twig => {
+        twig
+    });
+
+}
+
+var rootId = 1
+function rootTree(g, rootId){
+    let root = Treenode(rootId, null, [])
+    return GrowTree(g, root, null)
+}*/
+
+
+export function TreeMaker(){
+    //Get tree root node
+    console.log(AmorphusTreeEntity)
+}
+
+/*export {
+    AmorphusTreeEntity,
+    TreeMaker
+}*/

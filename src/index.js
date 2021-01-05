5730060f55d55c3807513e0e92bf3938d5d200a7
Dominic Martindale <awalletsdream@gmail.com>
2021-01-05 20:48:19 +1300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
 array in = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"]
 array out = {"Wheel": {_: {2,3,4}, Base}, Steering Wheel}
 */
+
+testArray = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"]
+
+//find longest common prefix and make a tree like structure from them.
+function findlcp(graph, node){
+
+}

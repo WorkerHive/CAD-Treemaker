0bf063d30e78bf47ffe612a51d92d3b997370a95
Dominic Martindale <awalletsdream@gmail.com>
2021-01-07 15:19:09 +1300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,58 +2,63 @@
 array in = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"]
 array out = {"Wheel": {_: {2,3,4}, Base}, Steering Wheel}
 */
-testArray = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"]
-//our list of prefixes
-var prefixes = []
-//compare string
-var cmpStr = ""
+
 //the parts of the array already accessed
-var readSlices = []
+//var readSlices = []
+
+const testArray = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"]
+var prefixes = []
+var cmpStr = "" //compare string
 var rootId = 0
 
-function rootNode(array, rootId){
-    let rootSlice = array[rootId]
-    console.log("root slice:", rootSlice)
-    return findlcp(array, rootSlice, cmpStr)
-}
-
 //find longest common prefix and make a tree like structure from them.
-function findlcp(array, node, cmpStr){
+function findlcp(array, cmpStr){
 
+    //if the compare string is blank, make one from first index of list
     if (cmpStr == "" || cmpStr == null) {
-        //set to node val
-        var lcp = node
-
-        //add cmpStr to "read list"
-        readSlices.push({"val":cmpStr, "instances":0})
+        //set cmpStr to first entry in array
+        var cmpStr = array[0]
+        console.log("New LCP candidate due to null cmpStr:", lcp)
     }
-    
-    //get longest prefix
-    console.log("slice:", node)
-    
 
-    //check through data for a prefix match, add if true
-    array.forEach(nodeSlice => {
-    let len = nodeSlice.length
-    console.log("lcp candidate:", cmpStr)
-        //set string if match
-        console.log("test: ",nodeSlice ," == ", node.slice(0, len), "?")
-        if(nodeSlice == node.slice(0, len)) {
-            lcp = nodeSlice //new potential lcp
-                //add lcp to 
+    //loop through data for a prefix match
+    array.forEach(e => {
+        let len = e.length
+        console.log("lcp candidate:", cmpStr)
+        console.log("test: [cmpStr] ",cmpStr ," == [slice]", e.slice(0, len), "?")
+
+        //check for match
+        if(cmpStr == e.slice(0, len)) {
+            //lcp = nodeSlice //new potential lcp
+            
+            //if another string found, add as prefix, build tree
+
+            //add lcp to list with count of 1
+
+
             console.log("result: yes")
-            console.log("success! Next word. \n")
+            console.log("success! Attempting loop. \n")
         } else {
             //lcp didnt match
             console.log("result: no")
             console.log("reducing tested string by last char")
-            lcp = nodeSlice.substring(0, len - 1);
-            console.log("new slice:", lcp)
-            findlcp(array, nodeSlice, lcp)
+
+            //reduce compared prefix by 
+            cmpStr = cmpStr.substring(0, (cmpStr.length - 1));
+            console.log("new slice:", e)
+            console.log("new string:", cmpStr)
+            findlcp(array, e, cmpStr)
         }
     });
-    console.log("longest common prefix: ", lcp)
 }
 
-//Run example
-var myGraph = rootNode(testArray, rootId, null);
+//Example
+//var testData = testArray.sort() //sometimes may be quicker to sort through alphabetically first for long lists
+var testData = testArray
+console.log("my test data:", testData)
+
+var newData = {}
+                                                    var myGraph = findlcp(testData, "");
+
+//Look at this photoGRAPH
+console.log("Finished LCP list: ", myGraph)

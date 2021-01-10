956b2b6518d33010800f5a83b894f92b923d753e
Dominic Martindale <awalletsdream@gmail.com>
2021-01-10 13:12:11 +1300

    Loop-de-Loop
    This is a super ineffecient algorithm something like O(arr.len^arr.len)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ array out = {"Wheel": {_: {2,3,4}, Base}, Steering Wheel}
 */
 
 //the parts of the array already accessed
-//var readSlices = []
+var readSlices = []
 
 const testArray = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"]
 var prefixes = []
@@ -12,38 +12,51 @@ var cmpStr = "" //compare string
 var rootId = 0
 
 //find longest common prefix and make a tree like structure from them.
-function findlcp(array, cmpStr){
+function findlcp(array, index, cmpStr){
 
-    //if the compare string is blank, make one from first index of list
-    if (cmpStr == "" || cmpStr == null) {
+    //if the compare string is blank, make one from the index of list
+    if (cmpStr == "" || cmpStr == null || cmpStr ) {
         //set cmpStr to first entry in array
-        var cmpStr = array[0]
-        console.log("New LCP candidate due to null cmpStr:", lcp)
+        var cmpStr = array[index]
+        console.log("Blank cmpStr set:", cmpStr, "\n")
     }
 
     //loop through data for a prefix match
-    array.forEach(e => {
+    array.forEach(function(e, index, array) {
         let len = e.length
-        console.log("lcp candidate:", cmpStr)
         console.log("test: [cmpStr] ",cmpStr ," == [slice]", e.slice(0, len), "?")
 
         //check for match
         if(cmpStr == e.slice(0, len)) {
-            //lcp = nodeSlice //new potential lcp
             
             //if another string found, add as prefix, build tree
-
-            //add lcp to list with count of 1
-
-
             console.log("result: yes")
-            console.log("success! Attempting loop. \n")
-        } else {
-            //lcp didnt match
-            console.log("result: no")
-            console.log("reducing tested string by last char")
+            console.log("outcome: Attempting loop.\n")
 
-            //reduce compared prefix by 
+            let matchIndex = []
+            //copy array
+            let arr2 = array
+            //remove entry at index
+            arr2.splice(index, 1)
+            console.log("new arr:", arr2)
+
+            //loop through the rest of the array to try and find another match
+            arr2.forEach(e2 => {
+                console.log("Nested comparison on: [cmpStr] ",cmpStr ," == [slice]", e2.slice(0, len), "?")
+                if(cmpStr == e2.slice(0, len)){
+                    console.log("result: yes, valid prefix found")
+                    //e slice is a valid prefix as 
+                } else {
+                    console.log("result: no")
+                }
+            })
+
+        } else {
+            //cmpstr didnt match the slice
+            console.log("result: no")
+            console.log("outcome: reducing tested string by last char")
+
+            //reduce compared prefix by 1 char
             cmpStr = cmpStr.substring(0, (cmpStr.length - 1));
             console.log("new slice:", e)
             console.log("new string:", cmpStr)
@@ -58,7 +71,7 @@ var testData = testArray
 console.log("my test data:", testData)
 
 var newData = {}
-                                                    var myGraph = findlcp(testData, "");
+var myGraph = findlcp(testData, 0, "");
 
 //Look at this photoGRAPH
 console.log("Finished LCP list: ", myGraph)

fea44cbf3cc0eb77221273ee664606fc71ff3980
Dominic Martindale <awalletsdream@gmail.com>
2021-01-12 17:42:54 +1300

    Nested obj sort
    use objects to ensure the match count is accounted for. The most matched prefix for each will be put into the lngMtchLst for duplicate sorting etc

--- src/index.js
+++ src/index.js
@@ -2,76 +2,87 @@
 array in = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"]
 array out = {"Wheel": {_: {2,3,4}, Base}, Steering Wheel}
 */
+const testArray = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"];
 
-//the parts of the array already accessed
-var readSlices = []
+var readSlices = []; //the parts of the array already converted completely into prefix groups
+var prefixes = [];
+var cmpStr = ""; //compare string
+var rootId = 0;
+var sliceIndex = 0; //the index of the substring slice for the cmpStr
 
-const testArray = ["Wheel_2", "Wheel_3", "Wheel_4", "Steering Wheel", "WheelBase"]
-var prefixes = []
-var cmpStr = "" //compare string
-var rootId = 0
+var mtchLst = []; //our list of matches for different prefixes of this element
+var lngMtchLst = []; //the most matched prefixes get put here for sorting
 
-//find longest common prefix and make a tree like structure from them.
-function findlcp(array, index, cmpStr){
-
-    //if the compare string is blank, make one from the index of list
+//compare contents of an array to build a tree of prefixes
+function growLCPTree(array, index, cmpStr){
+    //if the compare string is blank, make one from the first index of list
     if (cmpStr == "" || cmpStr == null || cmpStr ) {
-        //set cmpStr to first entry in array
-        var cmpStr = array[index]
+        var cmpStr = array[index].slice(0, 2) //get first 2 chars of index of the array
         console.log("Blank cmpStr set:", cmpStr, "\n")
     }
 
-    //loop through data for a prefix match
-    array.forEach(function(e, index, array) {
-        let len = e.length
-        console.log("test: [cmpStr] ",cmpStr ," == [slice]", e.slice(0, len), "?")
+    //go through all of the array comparing the first 2 characters
+    array.forEach(function(e, index, array){
+        let len = cmpStr.length;
+        console.log("test: [cmpStr] ",cmpStr ," == [slice]", e.slice(0, len), "?");
 
         //check for match
         if(cmpStr == e.slice(0, len)) {
-            
-            //if another string found, add as prefix, build tree
-            console.log("result: yes")
-            console.log("outcome: Attempting loop.\n")
+            console.log("\nMatch found, commencing length check")
 
-            let matchIndex = []
-            //copy array
-            let arr2 = array
-            //remove entry at index
-            arr2.splice(index, 1)
-            console.log("new arr:", arr2)
+            let arr2 = array;
+            let eleCmp = array[index];
+            arr2.splice(index, 1);
 
-            //loop through the rest of the array to try and find another match
+            //add the match obj to the match list
+            mtchLst.push(
+                {
+                    "element": e, //ele from the main data
+                    "index": index, //index of the ele from main data
+                    "prefixes" : [
+                        {
+                            "prefix": cmpStr,
+                            "matches": 1
+                        }
+                    ]
+                }
+            )
+
+            console.log(mtchLst[0].prefixes)
+
+            console.log("Finding LCP of: ", eleCmp)
+
             arr2.forEach(e2 => {
-                console.log("Nested comparison on: [cmpStr] ",cmpStr ," == [slice]", e2.slice(0, len), "?")
+                console.log("Nested test: [cmpStr] ",cmpStr ," == [slice]", e2.slice(0, len), "?");
                 if(cmpStr == e2.slice(0, len)){
                     console.log("result: yes, valid prefix found")
-                    //e slice is a valid prefix as 
+                    //e slice is a valid prefix as there was more than 1 match
+
+                    //add prefix as new key to list of prefixes
+                    console.log("Adding prefix: ", cmpStr)
+                    prefixes.push({cmpStr})
                 } else {
                     console.log("result: no")
                 }
             })
 
         } else {
-            //cmpstr didnt match the slice
-            console.log("result: no")
-            console.log("outcome: reducing tested string by last char")
-
-            //reduce compared prefix by 1 char
-            cmpStr = cmpStr.substring(0, (cmpStr.length - 1));
-            console.log("new slice:", e)
-            console.log("new string:", cmpStr)
-            findlcp(array, e, cmpStr)
+            //loop at end with no match, add arr index as unique and remove from list
+            if (index === array.length - 1) {
+                let unq = array[index]
+                prefixes.push({unq})
+                console.log("Adding element as unique: ", unq)
+            }
         }
-    });
+        
+        //if there are still elements in the list
+        
+    })
 }
 
 //Example
 //var testData = testArray.sort() //sometimes may be quicker to sort through alphabetically first for long lists
 var testData = testArray
 console.log("my test data:", testData)
-
-var newData = {}
-var myGraph = findlcp(testData, 0, "");
-
-//Look at this photoGRAPH
-console.log("Finished LCP list: ", myGraph)
+growLCPTree(testData, 0, "");
+console.log("Finished prefixes list: ", prefixes)
